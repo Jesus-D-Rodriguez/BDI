e089c4c0334085c6d17b5cb0cacf09edf197521b
jesus <jdrch62@gmail.com>
2022-12-28 00:37:40 -0400

    CRUD de equipo

--- src/app/routes/furbo.js
+++ src/app/routes/furbo.js
@@ -1,5 +1,6 @@
 const dbConnection = require("../../config/dbConnections.js");
 
+
 module.exports = app => {
     const connection = dbConnection();
 
@@ -22,6 +23,46 @@ module.exports = app => {
             res.redirect('/');
         });
 
+    });
+
+    app.get('/edit/:id', (req, res)=> {
+        const id = req.params.id;
+        connection.query('SELECT * FROM Equipo WHERE idEquipo=?', [id], (error, results)=> {
+            if (error) {
+                console.log("edit");
+                throw error;
+            } else {
+                
+                res.render('furbo/edit.ejs', {equipo:results[0]});
+            }
+        })
+    });
+
+    app.post('/update', (req, res) => {
+        const idEquipo = req.body.idEquipo;
+        const nombreEquipo = req.body.nombreEquipo;
+        console.log(req.body);
+        connection.query('UPDATE Equipo SET ? WHERE idEquipo = ?', [{nombreEquipo:nombreEquipo}, idEquipo], (error, result)=>{
+            if(error) {
+                throw error;
+            } else {
+                res.redirect('/');
+            }
+        });
+
+    });
+
+    //Ruta para eliminar equipo
+    app.get('/delete/:id', (req, res)=> {
+        const id = req.params.id;
+        connection.query('DELETE FROM Equipo WHERE idEquipo = ?', [id], (error, result)=> {
+            if (error) {
+                //throw error;
+            } else {
+                res.redirect('/');
+            }
+        } )
     })
+
 
 }
